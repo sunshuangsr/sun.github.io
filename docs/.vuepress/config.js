df2bcc18fae392fa96d9a1d37d7a20ed60ee62db
sunshuang <sunshuang@shrong.cn>
2022-11-17 15:01:30 +0800

    first change

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,14 +4,34 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "vdoing-template",
+  title: "sun.github.io",
   description: 'vdoing博客主题模板',
-  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/sun.github.io/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
 
   head,
-  plugins,
+  // vssue 评论插件
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "62267e2715f3930c364a",
+        clientSecret: "c9c778a8fd3a5cbb07e36431be30a2af826552fb",
+        owner: "sun",
+        repo: "sun.github.io",
+      },
+    ],
+  ],
+
   themeConfig,
+
 }
+
+
